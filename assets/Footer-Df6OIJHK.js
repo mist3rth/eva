eb1e2781c61507aff6984cfa488fcebe72dc21a3
mist3rth <mist3rth@users.noreply.github.com>
2026-05-21 15:20:04 +0200

    Updates

--- assets/Footer-Df6OIJHK.js
+++ assets/Footer-Df6OIJHK.js
@@ -1,4 +1,4 @@
-import{c as o,j as e,m as n}from"./index-CBCXb6F7.js";import{L as c,F as d}from"./facebook-cVxTow2w.js";/**
+import{c as o,j as e,m as n}from"./index-DmUKYiAd.js";import{L as c,F as d}from"./facebook-C-mOLIpb.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
